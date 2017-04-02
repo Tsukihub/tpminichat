7e816d4d61d6530b114b167e087dc5b445798fb0
Tsukihub <audeeccher@gmail.com>
2017-04-02 19:34:33 +0200

    push avant envoie à ocs

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
+<?php
+$pseudo='Entrez votre pseudonyme';
+if (!isset($_COOKIE['cookiepseudo'])){
+	setcookie('cookiepseudo', $pseudo, time() + 365*24*3600, null, null, false, true);
+	}
+?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="style/style.css">
-	<title>minichat</title>
+	<title>Document</title>
 </head>
 <body>
 
 <!-- formulaire  -->
 <form action="minichat_post.php" method="post" id="minichat">
-	<fieldset>
-    	<legend>Mon petit chat</legend>
 		<label for="pseudo"> Pseudo:
-			<input type="text" id="pseudo" name="pseudo" value="<?php if(isset($_COOKIE['cookiepseudo'])){echo $_COOKIE['cookiepseudo'];} ?>" placeholder="entrez votre pseudonyme">
+			<input type="text" id="pseudo" name="pseudo" value="<?= $_COOKIE['cookiepseudo'] ?>" placeholder="entrez votre pseudonyme">
 		</label></br>
 		<label for="Message">Message: </label>
 		<textarea form ="minichat" name="message"  cols="90" wrap="soft"></textarea></br>
 		<input type="submit" value="Envoyer">
-	</fieldset>
-</form>	
-
-
-
+	</form>	
 <!-- affichage du contenu de la bdd -->
 <?php
 
@@ -37,12 +37,12 @@
 		DESC LIMIT 10");
 
 	$req->execute();
-	echo '<section class="textechat" >';
+	echo '<ul>';
 	while ($donnees = $req->fetch())
 	{
 		echo '</br>'. $donnees['dateMessage'] .'<strong> '. htmlspecialchars($donnees['pseudo']) . '</strong> : </br>' . htmlspecialchars($donnees['message']) . '</br>';
 	}
-	echo '</section>';
+	echo '</ul>';
 
 	$req->closeCursor();
 	
--- minichat_post.php
+++ minichat_post.php
@@ -7,7 +7,7 @@ if (isset($_POST['pseudo']) && !empty($_POST['pseudo'])){
 	$pseudo=htmlspecialchars($_POST['pseudo']);
 
 	//////////////////écrase le cookie si le pseudo de l'utilisateur change ou si cookie = cookie par défault/////////////////////////////////////////////
-	if ($_COOKIE['cookiepseudo']=='Entrez votre pseudonyme' OR $_COOKIE['cookiepseudo']!==$_POST['pseudo']){
+	if ($_COOKIE['cookiepseudo']!==$_POST['pseudo']){
 	setcookie('cookiepseudo', $pseudo, time() + 365*24*3600, null, null, false, true);
     }
 
@@ -15,7 +15,7 @@ if (isset($_POST['pseudo']) && !empty($_POST['pseudo'])){
 
 
 
-	if (isset($_POST['message']) && !empty($_POST['message']) && $_COOKIE['cookiepseudo']!=='Entrez votre pseudonyme'){
+	if (isset($_POST['message']) && !empty($_POST['message'])){
 		$message=htmlspecialchars($_POST['message']);
 
 		////////////////envoie vers bdd si tous les champs ont été remplis par l'utilisateur/////////////////////////////////////////////
@@ -26,7 +26,7 @@ if (isset($_POST['pseudo']) && !empty($_POST['pseudo'])){
 		'dateMessage' => date("Y-m-d H:i:s")
 		));
 		$req->closeCursor();
-		header('Location: minichat.php');
+		header('Location: index.php');
 
 
 
